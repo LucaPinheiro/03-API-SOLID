1b25b4ba9651f292e4874b50528db5976dc51395
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-20 17:36:55 -0300

    feat: Add create method to GymsRepository interface for gym data creation

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from "@prisma/client";
+import { Gym, Prisma } from "@prisma/client";
 
 export interface GymsRepository {
   findById(id: string): Promise<any>;
+  create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
