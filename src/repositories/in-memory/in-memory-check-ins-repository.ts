ecf0ea5d29280a87ea5b9b05d3cb479b56f6be2a
LucaPinheiro <lucapgomes11@gmail.com>
2024-07-24 22:16:45 -0300

    feat: Add countByUserId method to InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,11 @@ import dayjs from "dayjs";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async countByUserId(userId: string): Promise<number> {
+    return Promise.resolve(
+      this.items.filter((item) => item.user_id === userId).length
+    );
+  }
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
