4e7dc098eab30e476b4719e2ed99d9dc4c08e4b8
lucapinheiro <lucapgomes11@gmail.com>
2024-08-09 23:58:48 -0300

    Update fastify-jwt type declaration to include 'role' property

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,9 +1,10 @@
-import fastifyJwt from "@fastify/jwt";
+import '@fastify/jwt'
 
-declare module "fastify" {
+declare module '@fastify/jwt' {
   export interface FastifyJWT {
     user: {
-      sub: string;
-    };
+      role: 'ADMIN' | 'MEMBER'
+      sub: string
+    }
   }
 }
