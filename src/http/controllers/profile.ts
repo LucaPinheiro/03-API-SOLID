f3f7dd4ac6627cdf4117589a2f0dc6d538cfe7a2
lucapinheiro <lucapgomes11@gmail.com>
2024-08-09 23:58:52 -0300

    feat: Update profile controller to retrieve user profile and remove password_hash from response

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,16 @@
+import { makeGetUserProfileUsecase } from "@/usecases/factories/make-get-user-profile-usecase";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  
-  await request.jwtVerify()
+
+  const getUserProfile = makeGetUserProfileUsecase();
+
+  const { user } = await getUserProfile.execute({ userId: request.user.sub });
 
 
-  return reply.status(200).send();
+
+  return reply.status(200).send({ user: {
+    ...user,
+    password_hash: undefined,
+  } });
 }
