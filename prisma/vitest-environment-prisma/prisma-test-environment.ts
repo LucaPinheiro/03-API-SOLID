824995227b5603b12f442e77b4068ccc8fb33f6f
lucapinheiro <lucapgomes11@gmail.com>
2024-08-10 00:30:23 -0300

    feat: Add prisma test environment configuration

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+
+import { Environment } from "vitest";
+
+export default <Environment>{
+  name: "prisma",
+  transformMode: "web",
+  async setup() {
+    console.log("setup");
+    return {
+      async teardown() {
+        console.log("teardown");
+      },
+    };
+  },
+};
