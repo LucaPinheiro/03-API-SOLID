543c415f4073112b3d44dfb8bf959d25da026f12
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 13:31:04 -0600

    feat: Update prisma test environment setup

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,40 @@
 import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "crypto";
 
 import { Environment } from "vitest";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// postgresql://docker:docker@localhost:5432/gympass-node-mss?schema=public
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL not found");
+  }
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "web",
   async setup() {
-    console.log("setup");
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+    process.env.DATABASE_URL = databaseUrl;
+    execSync("npx prisma migrate deploy");
+
     return {
       async teardown() {
-        console.log("teardown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       },
     };
   },
