9b4762ba70943e398168570d045b604ab45643c4
lucapinheiro <lucapgomes11@gmail.com>
2024-08-09 23:32:34 -0300

    feat: Add JWT verification to profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,6 +1,9 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  
+  await request.jwtVerify()
+
+
   return reply.status(200).send();
 }
