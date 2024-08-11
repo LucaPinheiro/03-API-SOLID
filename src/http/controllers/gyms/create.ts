333f524357ba2172890953fb44bfeb383964bddc
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 23:00:28 -0600

    refactor: Update gym create controller to use consistent naming convention

--- src/http/controllers/gyms/create.ts
+++ src/http/controllers/gyms/create.ts
@@ -1,6 +1,6 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import { z } from "zod";
-import { makeCreateGymUsecase } from "@/usecases/factories/make-create-gym-usecase";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeCreateGymUsecase } from '@/usecases/factories/make-create-gym-usecase'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createGymBodySchema = z.object({
@@ -8,25 +8,25 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     description: z.string().nullable(),
     phone: z.string().nullable(),
     latitude: z.number().refine((value) => {
-      return Math.abs(value) <= 90;
+      return Math.abs(value) <= 90
     }),
     longitude: z.number().refine((value) => {
-      return Math.abs(value) <= 180;
+      return Math.abs(value) <= 180
     }),
-  });
+  })
 
   const { title, description, phone, latitude, longitude } =
-    createGymBodySchema.parse(request.body);
+    createGymBodySchema.parse(request.body)
 
-  const createGymUsecase = makeCreateGymUsecase();
+  const createGymUseCase = makeCreateGymUsecase()
 
-  await createGymUsecase.execute({
+  await createGymUseCase.execute({
     title,
     description,
     phone,
     latitude,
     longitude,
-  });
+  })
 
-  return reply.status(201).send({ message: "Gym created" });
+  return reply.status(201).send()
 }
