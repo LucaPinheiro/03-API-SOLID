7aa97ea636645ab22ca26ef51721d2b246133dfe
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 23:00:42 -0600

    refactor: Update search gyms controller to use query parameters instead of request body

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -1,28 +1,23 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import { z } from "zod";
-import { makeCreateGymUsecase } from "@/usecases/factories/make-create-gym-usecase";
-import { SearchGymsUsecase } from "@/usecases/search-gyms";
-import { makeSearchGymsUsecase } from "@/usecases/factories/make-search-gyms-usecase";
-import { makeFetchNearbyGymsUsecase } from "@/usecases/factories/make-fetch-nearby-usecase";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUsecase } from '@/usecases/factories/make-search-gyms-usecase'
 
-export async function nearby(request: FastifyRequest, reply: FastifyReply) {
-  const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
-      return Math.abs(value) <= 90;
-    }),
-    longitude: z.number().refine((value) => {
-      return Math.abs(value) <= 180;
-    }),
-  });
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
 
-  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query);
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
 
-  const nearbyGymsUsecase = makeFetchNearbyGymsUsecase();
+  const searchGymsUseCase = makeSearchGymsUsecase()
 
-  const { gyms } = await nearbyGymsUsecase.execute({
-    userLatitude: latitude,
-    userLongitude: longitude,
-  });
+  const { gyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  })
 
-  return reply.status(201).send({ gyms });
+  return reply.status(200).send({
+    gyms,
+  })
 }
