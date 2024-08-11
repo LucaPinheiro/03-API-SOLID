0c1813b68ced583e34509aef653e44bbdf0db6b4
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 23:25:57 -0600

    refactor: Update check-ins history controller to use new fetch user check-ins history use case

--- src/http/controllers/check-ins/history.ts
+++ src/http/controllers/check-ins/history.ts
@@ -1,26 +1,22 @@
-import { FastifyRequest, FastifyReply } from "fastify";
-import { z } from "zod";
-import { makeCreateGymUsecase } from "@/usecases/factories/make-create-gym-usecase";
-import { SearchGymsUsecase } from "@/usecases/search-gyms";
-import { makeSearchGymsUsecase } from "@/usecases/factories/make-search-gyms-usecase";
-import { makeFetchNearbyGymsUsecase } from "@/usecases/factories/make-fetch-nearby-usecase";
-import { makeFetchUserCheckInHistoryUsecase } from "@/usecases/factories/make-fetch-user-check-ins-usecase";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeFetchUserCheckInsHistoryUsecase } from '@/usecases/factories/make-fetch-user-check-ins-history'
 
 export async function history(request: FastifyRequest, reply: FastifyReply) {
   const checkInHistoryQuerySchema = z.object({
     page: z.coerce.number().min(1).default(1),
-  });
+  })
 
-  const { page } = checkInHistoryQuerySchema.parse(
-    request.query
-  );
+  const { page } = checkInHistoryQuerySchema.parse(request.query)
 
-  const checkInHistoryUsecase = makeFetchUserCheckInHistoryUsecase();
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUsecase()
 
-  const { checkIns } = await checkInHistoryUsecase.execute({
-    userId: request.user.sub, 
+  const { checkIns } = await fetchUserCheckInsHistoryUseCase.execute({
     page,
-  });
+    userId: request.user.sub,
+  })
 
-  return reply.status(201).send({ checkIns });
+  return reply.status(200).send({
+    checkIns,
+  })
 }
