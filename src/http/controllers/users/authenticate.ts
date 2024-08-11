f81cb0ed783ba877bd7652e46c5f03f2e5f60add
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 21:29:35 -0600

    refactor: Add user authentication controller and endpoint

---
+++ src/http/controllers/users/authenticate.ts
@@ -0,0 +1,41 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { InvalidCredentialsError } from "@/usecases/errors/invalid-credentials-error";
+import { makeAuthenticateUsecase } from "@/usecases/factories/make-authenticate-usecase";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const authenticateUsecase = makeAuthenticateUsecase();
+
+    const { user } = await authenticateUsecase.execute({
+      email,
+      password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+    return reply.status(200).send({ token });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message });
+    }
+
+    throw err;
+  }
+}
