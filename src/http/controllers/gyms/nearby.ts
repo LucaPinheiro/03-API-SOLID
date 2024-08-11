b66950f29664221ffd778a88a5ec6482853d4d65
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-10 21:43:13 -0600

    feat: Add nearby gyms search controller and endpoint

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,23 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+import { makeCreateGymUsecase } from "@/usecases/factories/make-create-gym-usecase";
+import { SearchGymsUsecase } from "@/usecases/search-gyms";
+import { makeSearchGymsUsecase } from "@/usecases/factories/make-search-gyms-usecase";
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { q, page } = searchGymsQuerySchema.parse(request.body);
+
+  const searchGymsUsecase = makeSearchGymsUsecase();
+
+  const { gyms } = await searchGymsUsecase.execute({
+    query: q,
+    page,
+  });
+
+  return reply.status(201).send({ gyms });
+}
