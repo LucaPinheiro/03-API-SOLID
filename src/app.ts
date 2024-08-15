44cb6f1108d5b7d134b6ff80780393f235f0d262
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-14 20:28:50 -0600

    refactor: Update check-in routes to use plural form

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "@/env";
-import fastifyJwt from "@fastify/jwt";
-import { userRoutes } from "./http/controllers/users/routes";
-import { gymsRoutes } from "./http/controllers/gyms/routes";
-import { checkInRoutes } from "./http/controllers/check-ins/routes";
-import fastifyCookie from "@fastify/cookie";
+import { userRoutes } from "@/http/controllers/users/routes";
+import { gymsRoutes } from "@/http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 
@@ -24,7 +24,7 @@ app.register(fastifyCookie);
 
 app.register(userRoutes);
 app.register(gymsRoutes);
-app.register(checkInRoutes);
+app.register(checkInsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -36,7 +36,6 @@ app.setErrorHandler((error, _, reply) => {
   if (env.NODE_ENV !== "production") {
     console.error(error);
   } else {
-    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
   }
 
   return reply.status(500).send({ message: "Internal server error." });
