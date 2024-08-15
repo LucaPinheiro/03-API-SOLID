f209d4bd1ffa3dd5267806214fbc258134e69926
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-14 20:29:10 -0600

    refactor: Update gymsRoutes function to use single quotes consistently

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,22 +1,16 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from 'fastify'
 
-import { verifyJWT } from "../../middlewares/verify-jwt";
-import { search } from "./search";
-import { nearby } from "./nearby";
-import { create } from "./create";
-import { verifyUserRole } from "@/http/middlewares/verify-user-role";
-import { validate } from "../check-ins/validate";
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymsRoutes(app: FastifyInstance) {
-  app.addHook("onRequest", verifyJWT);
+  app.addHook('onRequest', verifyJWT)
 
-  app.get("/gyms/search", search);
-  app.get("/gyms/nearby", nearby);
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 
-  app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")] }, create);
-  app.patch(
-    "/check-ins/:checkInId/validate",
-    { onRequest: [verifyUserRole("ADMIN")] },
-    validate
-  );
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
