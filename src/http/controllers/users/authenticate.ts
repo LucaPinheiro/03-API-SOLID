63dbff1505753ca1056fc1954b579ddf4b5cc062
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-14 20:29:17 -0600

    refactor: Update authenticate function to use single quotes consistently

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,26 +1,26 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
-import { InvalidCredentialsError } from "@/usecases/errors/invalid-credentials-error";
-import { makeAuthenticateUsecase } from "@/usecases/factories/make-authenticate-usecase";
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { InvalidCredentialsError } from '@/usecases/errors/invalid-credentials-error'
+import { makeAuthenticateUsecase } from '@/usecases/factories/make-authenticate-usecase'
 
 export async function authenticate(
   request: FastifyRequest,
-  reply: FastifyReply
+  reply: FastifyReply,
 ) {
   const authenticateBodySchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
-  });
+  })
 
-  const { email, password } = authenticateBodySchema.parse(request.body);
+  const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
-    const authenticateUsecase = makeAuthenticateUsecase();
+    const authenticateUseCase = makeAuthenticateUsecase()
 
-    const { user } = await authenticateUsecase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
-    });
+    })
 
     const token = await reply.jwtSign(
       {
@@ -30,8 +30,8 @@ export async function authenticate(
         sign: {
           sub: user.id,
         },
-      }
-    );
+      },
+    )
 
     const refreshToken = await reply.jwtSign(
       {
@@ -40,25 +40,27 @@ export async function authenticate(
       {
         sign: {
           sub: user.id,
-          expiresIn: "7d",
+          expiresIn: '7d',
         },
-      }
-    );
+      },
+    )
 
     return reply
-      .setCookie("refreshToken", refreshToken, {
-        path: "/",
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
         secure: true,
         sameSite: true,
         httpOnly: true,
       })
       .status(200)
-      .send({ token });
+      .send({
+        token,
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
-      return reply.status(400).send({ message: err.message });
+      return reply.status(400).send({ message: err.message })
     }
 
-    throw err;
+    throw err
   }
 }
