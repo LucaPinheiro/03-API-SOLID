9fcbff68c9d1942847c1b03c69963851f0036a96
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-17 21:46:00 -0600

    refactor: Update email address in createAndAuthenticateUser function

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -10,14 +10,14 @@ export async function createAndAuthenticateUser(
   await prisma.user.create({
     data: {
       name: 'John Doe',
-      email: 'johndoe@example.com',
+      email: 'matue@30praum.com',
       password_hash: await hash('123456', 6),
       role: isAdmin ? 'ADMIN' : 'MEMBER',
     },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
-    email: 'johndoe@example.com',
+    email: 'matue@30praum.com',
     password: '123456',
   })
   const { token } = authResponse.body
